4ba8aecef215d10dd76de15e1ebe50f2db3f276b
PilkeySEK <PilkeySEK@gmx.de>
2024-09-14 09:40:04 +0200

    Fixed a bug that would kick the player if the resulting morse code message is too long for the chat (256+ chars), added a message stating that it is too long and there's an option to copy it instad

--- src/client/java/me/pilkeysek/morsecodechat/client/command/MorseCommand.java
+++ src/client/java/me/pilkeysek/morsecodechat/client/command/MorseCommand.java
@@ -8,6 +8,11 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class MorseCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
@@ -17,10 +22,37 @@ public class MorseCommand {
                         .then(
                                 ClientCommandManager.argument("message", StringArgumentType.greedyString())
                                         .executes(context -> {
+                                            String message = StringArgumentType.getString(context, "message");
+                                            String morseMessage = Util.stringToMorse(message);
+
+                                            if(morseMessage.length() > 256) {
+                                                context.getSource().sendFeedback(
+                                                        Text.translatable("chat.morsecodechat.morseTooLong")
+                                                                .formatted(Formatting.RED)
+                                                                .append(Text.translatable("chat.morsecodechat.copyInstead")
+                                                                        .setStyle(
+                                                                                Style.EMPTY.withClickEvent(
+                                                                                        new ClickEvent(
+                                                                                                ClickEvent.Action.COPY_TO_CLIPBOARD,
+                                                                                                morseMessage
+                                                                                        )
+                                                                                ).withHoverEvent(
+                                                                                        new HoverEvent(
+                                                                                                HoverEvent.Action.SHOW_TEXT,
+                                                                                                Text.translatable("chat.morsecodechat.copyTheMorse")
+                                                                                                        .formatted(Formatting.DARK_GREEN)
+                                                                                        )
+                                                                                )
+                                                                        )
+
+                                                                ));
+                                                return 0;
+                                            }
+
                                             ClientPlayNetworkHandler networkHandler = MinecraftClient.getInstance().getNetworkHandler();
                                             assert networkHandler != null;
 
-                                            networkHandler.sendChatMessage(Util.stringToMorse(StringArgumentType.getString(context, "message")));
+                                            networkHandler.sendChatMessage(morseMessage);
 
                                             return 1;
                                         })
